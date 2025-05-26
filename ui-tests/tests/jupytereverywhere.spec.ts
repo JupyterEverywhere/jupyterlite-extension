da19f481a661b8c9af06671c12e39b6d1e6b19a4
krassowski <5832902+krassowski@users.noreply.github.com>
2025-05-26 21:40:32 +0100

    Add snapshot for share dialog

--- ui-tests/tests/jupytereverywhere.spec.ts
+++ ui-tests/tests/jupytereverywhere.spec.ts
@@ -17,16 +17,29 @@ async function runCommnad(page: Page, command: string, args: JSONObject = {}) {
   );
 }
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('lab/index.html');
+  await page.waitForSelector('.jp-LabShell');
+});
+
 test.describe('General', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('lab/index.html');
-    await page.waitForSelector('.jp-LabShell');
-  });
   test('Should load the notebook', async ({ page }) => {
     await runCommnad(page, 'docmanager:new-untitled', { type: 'notebook' });
     await runCommnad(page, 'docmanager:open', { path: 'Untitled.ipynb' });
     expect(await page.locator('.jp-LabShell').screenshot()).toMatchSnapshot(
       'application-shell.png'
+    );
+  });
+});
+
+test.describe('Sharing', () => {
+  test('Should open share dialog', async ({ page }) => {
+    await runCommnad(page, 'docmanager:new-untitled', { type: 'notebook' });
+    await runCommnad(page, 'docmanager:open', { path: 'Untitled.ipynb' });
+    const shareButton = page.locator('.jp-ToolbarButton').getByLabel('Share');
+    await shareButton.click();
+    expect(await page.locator('.jp-Dialog-content').screenshot()).toMatchSnapshot(
+      'share-dialog.png'
     );
   });
 });
