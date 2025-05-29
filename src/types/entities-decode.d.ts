240cdfe98b5d19602b7205d79c4689a1b997e103
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-05-30 04:28:03 +0530

    Fix `EntityDecoder` type export

--- src/types/entities-decode.d.ts
+++ src/types/entities-decode.d.ts
@@ -1,1 +1,5 @@
-declare module 'entities/decode';
+declare module 'entities/decode' {
+  export type EntityDecoder = (input: string) => string;
+  const decode: EntityDecoder;
+  export default decode;
+}
