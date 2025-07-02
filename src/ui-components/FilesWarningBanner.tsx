b3d2ba4fd2112c78925bb68249085009d47b2b5a
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-07-02 07:10:03 +0530

    Lint some more

--- src/ui-components/FilesWarningBanner.tsx
+++ src/ui-components/FilesWarningBanner.tsx
@@ -7,10 +7,9 @@ export const FilesWarningBanner: React.FC = () => {
   return (
     <div className="je-WarningBanner">
       <div className="je-WarningBanner-content">
-        <strong>WARNING:</strong>{' '}
-        To save memory, all data and image files are discarded after each session.
-        Please upload your data each time you use your notebook.
-        Note: images rendered in the notebook will persist between sessions.
+        <strong>WARNING:</strong> To save memory, all data and image files are discarded after each
+        session. Please upload your data each time you use your notebook. Note: images rendered in
+        the notebook will persist between sessions.
       </div>
     </div>
   );
