ddbef6b6eee385c1880aef4a08dc9f96d27f7993
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-07-17 23:36:17 +0530

    Add a `LeaveConfirmation` dialogue

---
+++ src/ui-components/LeaveConfirmation.tsx
@@ -0,0 +1,22 @@
+import { ReactWidget } from '@jupyterlab/apputils';
+import React from 'react';
+
+/**
+ * A dialog widget that asks users if they want to share the notebook
+ * before navigating away from the current page.
+ */
+export class LeaveConfirmation extends ReactWidget {
+  render(): JSX.Element {
+    return (
+      <div className="je-LeaveDialog">
+        <p className="je-LeaveDialog-title">
+          Would you like to save the link to your notebook before navigating away?
+        </p>
+        <p className="je-LeaveDialog-note">
+          <strong>Note:</strong> To edit your work later, you’ll have to save the link to your
+          notebook and make a copy.
+        </p>
+      </div>
+    );
+  }
+}
