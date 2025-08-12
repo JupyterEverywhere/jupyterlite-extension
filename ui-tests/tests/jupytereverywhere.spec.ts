fb23cec78e117644f0dd37b5b74dfb254f3d1d4b
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-08-13 01:27:53 +0530

    Revert "Add timeout before "select kernel" text is updated"
    This reverts commit d65dde080248ecbb649438c54f1d65183dc9f24b.

--- ui-tests/tests/jupytereverywhere.spec.ts
+++ ui-tests/tests/jupytereverywhere.spec.ts
@@ -598,7 +598,6 @@ test.describe('Sharing and copying R and Python notebooks', () => {
 
     // Verify kernel is Python
     const kernelLabel = await page.locator('.je-KernelSwitcherButton').innerText();
-    await page.waitForTimeout(2000);
     expect(kernelLabel.toLowerCase()).toContain('python');
   });
 });
